b0738f8b21589380ea69d43786542105575cb1db
kushalsingh-00 <kushalsinghrathore7@gmail.com>
2020-10-31 20:31:09 +0530

    Activity Lifecycle & State

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.twoactivities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 
@@ -21,9 +23,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(LOG_TAG, "onCreate");
+
         message = ActivityMainBinding.inflate(getLayoutInflater());
         View view=message.getRoot();
         setContentView(view);
+
+        if(savedInstanceState!=null)
+        {
+            if(savedInstanceState.getBoolean("reply_visible")) {
+                message.textHeaderReply.setVisibility(View.VISIBLE);
+                message.textMessageReply.setText(savedInstanceState.getString("reply_text"));
+                message.textMessageReply.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(message.textHeaderReply.getVisibility()==View.VISIBLE)
+        {
+            outState.putBoolean("reply_visible",true);
+            outState.putString("reply_text", message.textMessageReply.getText().toString());
+        }
     }
 
     public void launchSecondActivity(View view) {
@@ -47,5 +71,41 @@ public class MainActivity extends AppCompatActivity {
                 message.textMessageReply.setVisibility(View.VISIBLE);
             }
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.twoactivities.databinding.ActivitySecondBinding;
@@ -11,6 +12,7 @@ import com.example.twoactivities.databinding.ActivitySecondBinding;
 public class SecondActivity extends AppCompatActivity {
     private ActivitySecondBinding binding;
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,44 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent=new Intent();
         replyIntent.putExtra(EXTRA_REPLY,reply);
         setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
+
         finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
